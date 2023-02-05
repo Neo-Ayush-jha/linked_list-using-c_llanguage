958f45380b18cc506b1f3145893c330b50bc0e49
Ayush Kumar Jha <ayush91176@gmail.com>
2023-02-06 01:29:12 +0530

    circular linked list

--- circular_linked_list.c
+++ circular_linked_list.c
@@ -1,5 +1,5 @@
 //documentation section--------------------------------------->
-/* PROGRAM FOR DOUBLE LINKED LIST */
+/* PROGRAM FOR CIRCLULAR LINKED LIST */
 //link section------------------------------------------------>
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,51 +12,51 @@ typedef struct node{
 typedef struct link_list{
 	struct node *head;
 }link_list;
+
 //definition section------------------------------------------>
 node *new_node(int data);
 link_list *new_link_list(int data);
 void show(link_list *l);
-void insert_frount(link_list *l, node *n);
-void insert_after(node *n,node *a);
+void insert_after(node *l, node *n);
 void del(link_list *l,node *n);
 
 //main function------------------------------------------------>
 int main(){
 	link_list *l= new_link_list(10);
 	
-	node *a,*b,*c;
+	node *a,*b,*c,*d;
 	
 	a=new_node(20);
 	b=new_node(30);
 	c=new_node(40);
-	
+	d=new_node(50);
 	l->head->next=a;
 	a->next=b;
 	b->next=c;
+	c->next=d;
+	show(l);
 	
 	node *z;
 	
 	z=new_node(9);
-	insert_frount(l,z);
+	insert_after(a,z);
 	show(l);
 	
 	z=new_node(99);
 	insert_end(l,z);
 	show(l);
-	
-	z=new_node(999);
-	insert_after(z,a);
-	show(l);
 		
-	del(l,b);
+	del(l,a);
 	show(l);
 }
+
+
 //SUBPROGRAM SECTION--------------------------------------------->
 node *new_node(int data){
 	node *z;
 	z=malloc(sizeof(struct node));
 	z->data=data;
-	z->next=NULL;
+	z->next=z;
 	return z;
 }
 
@@ -67,49 +67,44 @@ link_list *new_link_list(int data){
 	l->head=a;
 	return l;	
 }
-
 void show(link_list *l){
 	node *temp =l->head;
-	int i=1;
-	while(temp !=NULL){
-		
-		printf("%d = %d\t",i,temp->data);
+		int i=1;
+	do{
+	
+		printf("%d=%d\t",i,temp->data);
 		temp = temp->next;
 		i++;
-	}	
-	printf("\n");
+	}while(temp->next !=temp);
+	printf("complet \n");
 }
 
-void insert_frount(link_list *l, node *n){
-	n->next= l->head;
-	l->head->prev =n;
-	l->head=n;
+void insert_after(node *l,node *n){
+	n->next=l->next;
+	l->next=n;
+	n->prev=l;
+	l->prev=NULL;
 }
 
 void insert_end(link_list *l,node *n){
-	node *temp = l->head;
-	while(temp->next!= NULL){
-		temp = temp->next;
-	}
-	temp->next=n;
-	n->prev=temp;
+	
+	n->next=l->head->next;
+	l->head->next=n;
+	l->head=n; 
 }
 
-void insert_after(node *n,node *a){
-	n->next = a->next;
-	a->next->prev=n;
-	a->next=n;
-	n->prev=a;
-}
-
-void del(link_list *l,node *n){
-	if(n->prev!=NULL){
-		n->prev->next=n->next;
-	}
-	else if(n->next!=NULL){
-		n->next->prev =n->prev;
+void del(link_list *l, node *n){
+	node *temp =l->head;
+	if(n==l->head){
+		if(n==l->head->next){
+			l->head=NULL;
+		}
+		else{
+			temp->next=n->next;
+			l->head=temp;
+		}
 	}
 	else{
-		l->head=n->next;
+		temp->next=n->next;
 	}
 }
